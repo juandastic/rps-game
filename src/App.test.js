2a82e8b3e7cfd5eaffc146518269daf16e71ee1c
Juan David Gomez <juandastic@gmail.com>
2019-04-29 02:00:55 -0500

    Fix test and update README

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(
+  (state)=>state,
+  {
+    game: {},
+    board: [],
+    error: {}
+  }
+);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
